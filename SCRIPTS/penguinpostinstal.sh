2d24050c3be78b4cf3bea0f855a6a942198a08c4
Adrian bluefin <adrian.lulka@gmail.com>
2025-08-17 19:19:12 +0200

    penguinscript fully on root, maybe rapaired

--- SCRIPTS/penguinpostinstal.sh
+++ SCRIPTS/penguinpostinstal.sh
@@ -4,31 +4,37 @@
 # runas sudo only
 # checked deb 12/13
 
-pkg=(aria2 fd-find fzf gcc git grep gzip man mawk screenfetch neovim podman python3 pipx tmux unzip vim wget xclip curl stow bat 7zip jq btop)
+pkg=(aria2 fd-find fzf git grep gzip man mawk screenfetch podman python3 pipx tmux unzip vim wget xclip curl stow bat 7zip jq btop)
 
 apt update && apt upgrade -y && apt install ${pkg[@]}
 if [ $? -ne 0 ]; then
 	echo 'interrupted';exit 1
 fi
 
-pipx install tldr
 
 name="$(grep 1000 /etc/passwd | cut -d: -f1)"
-echo $name:100000:65536 >> /etc/subuid
-echo $name:100000:65536 >> /etc/subgid
- 
-[[ -f $HOME/.local/bin ]] || mkdir -p $HOME/.local/bin
-ln -s /bin/screenfetch $HOME/.local/bin/fastfetch
+userhome="/home/$name"
 
-[[ -d $HOME/mydotfiles ]] || cd $HOME && git clone http://github.com/asgf12/mydotfiles.git
-if [[ -d $HOME/mydotfiles && ! -h $HOME/.bashrc ]]; then
-	mkdir -p $HOME/.local/bak
-	[[ -f $HOME/.bashrc ]] && mv $HOME/.bashrc $HOME/.local/bak/
-	[[ -f $HOME/.bash_profile ]] && mv $HOME/.bash_profie $HOME/.local/bak/
-	[[ -f $HOME/.aliases ]] && mv $HOME/.aliases $HOME/.local/bak/
-	[[ -f $HOME/.bash_aliases ]] && mv $HOME/.bash_aliases $HOME/.local/bak/
-	[[ -d $HOME/.config/tmux ]] && mv $HOME/.config/tmux $HOME/.local/bak/
-	[[ -d $HOME/.config/containers ]] && mv $HOME/.config/containers $HOME/.local/bak/
+apt install tldr
+if [ $? -ne 0 ]; then
+    su - $name -c "pipx install tldr"
 fi
 
-[[ -h $HOME/.bashrc ]] || cd $HOME/mydotfiles && stow bashrc podman tmux aliases && echo -e 'basic config symlinked\nsourcing not suported\nrestart or\n. $HOME/.bashrc'
+[ -z "$(grep $name /etc/subuid)" ] && echo $name:100000:65536 >> /etc/subuid
+[ -z "$(grep $name /etc/subgid)" ] && echo $name:100000:65536 >> /etc/subgid
+ 
+[[ -f $userhome/.local/bin ]] || mkdir -p $userhome/.local/bin
+ln -s /bin/screenfetch $userhome/.local/bin/fastfetch
+
+[[ -d $userhome/mydotfiles ]] || cd $userhome && git clone http://github.com/asgf12/mydotfiles.git
+if [[ -d $userhome/mydotfiles && ! -h $userhome/.bashrc ]]; then
+	mkdir -p $userhome/.local/bak
+	[[ -f $userhome/.bashrc ]] && mv $userhome/.bashrc $userhome/.local/bak/
+	[[ -f $userhome/.bash_profile ]] && mv $userhome/.bash_profie $userhome/.local/bak/
+	[[ -f $userhome/.aliases ]] && mv $userhome/.aliases $userhome/.local/bak/
+	[[ -f $userhome/.bash_aliases ]] && mv $userhome/.bash_aliases $userhome/.local/bak/
+	[[ -d $userhome/.config/tmux ]] && mv $userhome/.config/tmux $userhome/.local/bak/
+	[[ -d $userhome/.config/containers ]] && mv $userhome/.config/containers $userhome/.local/bak/
+fi
+
+[[ -h $userhome/.bashrc ]] || cd $userhome/mydotfiles && stow bashrc podman tmux aliases && echo -e 'basic config symlinked\nsourcing not suported\nrestart or\n. $userhome/.bashrc'
