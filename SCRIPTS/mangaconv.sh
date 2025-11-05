bd904a9ca3f6fe80d08896dc656f403a2e3e1895
Adrian bluefin <adrian.lulka@gmail.com>
2025-11-05 21:27:10 +0100

    Changed output dir or something t di#

--- SCRIPTS/mangaconv.sh
+++ SCRIPTS/mangaconv.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 name=$(pwd | awk -F '/' '{print $NF}')
+mkdir Data
 
 fd -t d | while IFS= read -r l; do
     L=${l:0:-1}
     n=$name"_"$L".pdf"
     echo "processing: $n"
     cd "$l"
-    magick *.jpg -auto-orient "../$n"
+    magick *.jpg -auto-orient "../Data/$n"
     cd ..
 done
